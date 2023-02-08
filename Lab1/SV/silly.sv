0796f758679144a8ea4d36f6af9ee8ba607737b5
QuentinKoeninger <qkoenin@okstate.edu>
2023-02-08 16:47:48 -0600

    silly.sv and silly_tb.sv almost working, but not really

--- Lab1/SV/silly.sv
+++ Lab1/SV/silly.sv
@@ -1,5 +1,23 @@
-module silly (input  logic a, b, c, output logic y);
+module silly (input  logic a, b, cin, output logic y, cout);
    
-  assign y = ~b & ~c | a & ~b;
+  assign y = a ^ b ^ cin;
+  assign cout = (a & b) | (a & cin) | (b & cin);
    
 endmodule
+
+// Top-level module
+module sillyTest (input logic cin, [7:0]userInput, output logic [4:0]out);
+
+  logic [2:0]c;
+
+  silly bit0 (userInput[4], userInput[0], cin, out[0], c[0]);
+
+  silly bit1 (userInput[5], userInput[1], c[0], out[1], c[1]);
+
+  silly bit2 (userInput[6], userInput[2], c[1], out[2], c[2]);
+
+  silly bit3 (userInput[4], userInput[0], c[2], out[3], out[4]);
+
+endmodule
+
+  
--- Lab1/SV/silly_tb.sv
+++ Lab1/SV/silly_tb.sv
@@ -1,61 +1,70 @@
 `timescale 1ns / 1ps
 module tb ();
 
-   logic        a;
-   logic 	b;
-   logic 	c;
-   logic 	y;
-   logic        clk;   
+   logic    [7:0]bits;
+   logic 	carryIn;
+   logic    clk;
+   logic    [4:0]correctBits;
+   logic	[4:0]currentBits;
+   logic 	Error;
+   integer desc3;
+   integer handle3;
+   integer i;
    
   // instantiate device under test
-   silly dut (a, b, c, y);
+   sillyTest dut (carryIn, bits, currentBits);
  ////////////////////////////////////////////////////////////////////
    // 20 ns clock
    initial 
-     begin	
-	clk = 1'b1;
-	forever #10 clk = ~clk;
-     end
+    	begin	
+		clk = 1'b1;
+		forever #10 clk = ~clk;
+    end
+
+	initial
+	 begin
+		handle3 = $fopen("rca.out");
+		desc3 = handle3;
+		#1250 $finish;
+	 end
 
 
    initial
      begin
     
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b0;
+	#0   bits[7:0] = 8'b0000_0000;	
+	#0   carryIn = 1'b0;
+	#0	 correctBits = 5'b0_0000;
+	#0	 Error = currentBits != correctBits;
 
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
 
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
+	#20   bits[7:0] = 8'b0000_0001;	
+	#0   carryIn = 1'b0;
 
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b1;		
+	#20   bits[7:0] = 8'b0000_0010;	
+	#0   carryIn = 1'b0;
 
 
 	
      end
+
+	initial
+	 begin
+		for (i=0; i < 128; i=i+1)
+			begin
+			// Put vectors before beginning of clk
+			@(posedge clk)
+				begin
+					bits[7:4] = $random;
+					bits[3:0] = $random;
+					carryIn = $random;
+				end
+			@(negedge clk)
+				begin
+					$fdisplay(desc3, "%h %h || %h | %h | %b\n", bits[7:4], bits[3:0], currentBits, currentBits, (currentBits == currentBits));
+				end
+			end // @(negedge clk)
+		end
 
    
 endmodule
